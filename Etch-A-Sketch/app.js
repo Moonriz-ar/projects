e6759ca4e636b227b4a73ec998079cf3cc25ec2d
Andrea <andre93.lin@gmail.com>
2021-02-17 10:55:41 -0300

    added create drawing board function

--- Etch-A-Sketch/app.js
+++ Etch-A-Sketch/app.js
@@ -2,20 +2,24 @@
 let createDrawingBoard = function(gridQuantity) {
 
     let drawingBoard = document.getElementById('drawing-board');
+    let drawingBoardWidth = drawingBoard.offsetWidth;
+    let drawingBoardHeight = drawingBoard.offsetHeight;
 
-    for (let i = 0; i < gridQuantity; i++) {
-        let row = document.createElement('div');
-        row.className = 'row flex-default';
-    
         for (let i = 0; i < gridQuantity; i++) {
-            let square = document.createElement('div');
-            square.classList.add('square');
-            row.append(square);
-
-            //calculate width and height based on gridQuantity
-        }
-
-        drawingBoard.append(row);
-    }    
+            let row = document.createElement('div');
+            row.className = 'row flex-default';
+        
+            for (let i = 0; i < gridQuantity; i++) {
+                let square = document.createElement('div');
+                square.classList.add('square');
+                square.style.width = ((drawingBoardWidth / gridQuantity) - 2) + 'px';
+                square.style.height = ((drawingBoardHeight / gridQuantity) - 2) + 'px';
+                row.append(square);
+            }
+    
+            row.style.height = (100 / gridQuantity) + '%';
+            drawingBoard.append(row);
+        }    
 }
+
 
